78fd4e3ec1bbcb76e0cd9bda23ec2fa7ed261462
Ваше имяkiselov <kroliktin@gmail.com>
2016-05-26 13:38:51 +0300

    message

---
+++ CSharpHW/Hello_world/Hello_world/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello world");
+
+            Console.Read();
+
+        }
+    }
+}
